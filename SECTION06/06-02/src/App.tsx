7ae30e548d29fb3b8249c61b1a507942164b7f4c
YoungI422 <dlsrudtj@gmail.com>
2026-04-12 15:44:32 +0900

    feat: 6-2 강의 수강

--- SECTION06/06-02/src/App.tsx
+++ SECTION06/06-02/src/App.tsx
@@ -1,3 +1,15 @@
+import { useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [state, setState] = useState<number>(0); //useState를 활용하여 상태 데이터를 하나 정의함
+  const handleSetToTen = () => {
+    //state = 10; //상태 데이터는 직접적으로 수정할 수 없으므로 setState 함수를 이용하여 상태 데이터를 업데이트해야 함
+    setState(10); // 그럼 얘도 함수 정의를 한건가?
+  };
+  return (
+    <>
+      <h1>현재 카운트: {state}</h1>
+      <button onClick={handleSetToTen}> set To 10</button>
+    </>
+  );
 }
