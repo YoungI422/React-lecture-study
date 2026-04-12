e8c6ec7a02789cb39fbcffb58e95632f2aedf38e
YoungI422 <dlsrudtj@gmail.com>
2026-04-12 19:37:19 +0900

    feat: 7-9 강의 수강

--- SECTION07/07-09/src/App.tsx
+++ SECTION07/07-09/src/App.tsx
@@ -1,12 +1,17 @@
 export default function App() {
+  const fruits = ["apple", "banana", "orange"];
+  const item = [];
+  /*for (let i = 0; i < fruits.length; i++) {
+    item.push(<li>{fruits[i]}</li>);
+  }*/ // 반복 렌더링을 사용할 때는 key라는 props를 꼭 넣어줘야 한다. key는 고유한 값이어야 한다. 보통은 배열의 index를 넣어준다.
+  for (let i = 0; i < fruits.length; i++) {
+    item.push(<li key={i}>{fruits[i]}</li>);
+  }
   return (
     <>
       <p>Fruits Lists</p>
-      <ul>
-        <li>apple</li>
-        <li>banana</li>
-        <li>orange</li>
-      </ul>
+      {fruits}
+      <ul>{item}</ul>
     </>
   );
 }
