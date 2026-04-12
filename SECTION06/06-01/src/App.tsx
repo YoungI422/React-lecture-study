dc8fc30faa7f96b4f72e487153b1dc4fa9c8c103
YoungI422 <dlsrudtj@gmail.com>
2026-04-12 15:06:22 +0900

    feat: 06-01 수강

--- SECTION06/06-01/src/App.tsx
+++ SECTION06/06-01/src/App.tsx
@@ -1,3 +1,16 @@
 export default function App() {
-  return <></>;
+  let name = "영희";
+  let age = 20;
+  const handleClick = () => {
+    name = "철수";
+    age = 30;
+    console.log(name, age);
+  };
+  return (
+    <>
+      <p>이름: {name}</p>
+      <p>나이: {age}</p>
+      <button onClick={handleClick}> change </button>
+    </>
+  );
 }
