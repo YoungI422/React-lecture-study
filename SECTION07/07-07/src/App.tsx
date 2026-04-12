c5e20b82251d564512bca3225e120de3e58fc899
YoungI422 <dlsrudtj@gmail.com>
2026-04-12 19:27:24 +0900

    feat: 7-7 강의 수강

--- SECTION07/07-07/src/App.tsx
+++ SECTION07/07-07/src/App.tsx
@@ -1,3 +1,9 @@
 export default function App() {
-  return <></>;
+  const isLoggedIn = true;
+  return (
+    <>
+      {isLoggedIn && <h1>Welcome back!</h1>}
+      {!isLoggedIn && <h1>Please log in.</h1>}
+    </>
+  );
 }
