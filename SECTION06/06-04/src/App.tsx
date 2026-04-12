7718ee5bde2b89174edb22f9fc4549f248555a9a
YoungI422 <dlsrudtj@gmail.com>
2026-04-12 16:47:10 +0900

    feat: 6-5 강의 수강

--- SECTION06/06-04/src/App.tsx
+++ SECTION06/06-04/src/App.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    /* setCount(count + 1) // count + 1이 0 + 1로 계산되어 1이 됨;
+    setCount(count + 1) ; // count + 1이 0 + 1로 계산되어 1이 됨;
+    setCount(count + 1); */ // count + 1이 0 + 1로 계산되어 1이 됨;
+
+    setCount((count) => count + 1); // count + 1이 0 + 1로 계산되어 1이 됨;
+    setCount((count) => count + 1); // count + 1이 1 + 1로 계산되어 2가 됨;
+    setCount((count) => count + 1); // count + 1이 2 + 1로 계산되어 3이 됨;
+  };
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+    </>
+  );
 }
