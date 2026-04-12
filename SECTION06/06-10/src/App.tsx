158c2b2d846b80bb938d587afa30d5f8b430761f
YoungI422 <dlsrudtj@gmail.com>
2026-04-12 17:43:14 +0900

    feat: 6-10 강의 수강

--- SECTION06/06-10/src/App.tsx
+++ SECTION06/06-10/src/App.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [fruits, setFruits] = useState(["banana", "apple", "grape"]);
+  const handleAddFruit = () => {
+    // 배열요소를 참조해야 하기 때문에 함수로 만들어야 한다.
+    // setFruits((fruits) => [...fruits, "orange"]); // 배열의 마지막에 요소 추가
+    //setFruits((fruits) => ["mango", ...fruits]); // 배열의 처음에 요소 추가
+    //setFruits((fruits) =>
+    //  fruits.map((fruits) => (fruits === "banana" ? "strawberry" : fruits)),
+    //); // 배열의 요소 변경 map 함수
+    setFruits((fruits) => [...fruits.slice(0, 1), "mango", ...fruits.slice(1)]); // 배열의 요소 추가
+  };
+  return (
+    <>
+      <p> {fruits.join(", ")} </p>
+      <button onClick={handleAddFruit}> Add Fruit </button>
+    </>
+  );
 }
