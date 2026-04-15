8f869d51d10495d76472de74f95553e4fa1a7b9f
YoungI422 <dlsrudtj@gmail.com>
2026-04-16 01:16:09 +0900

    feat: 8-3 수강 완료

--- SECTION08/08-03/src/App.tsx
+++ SECTION08/08-03/src/App.tsx
@@ -1,8 +1,10 @@
 import Global from "./components/Global";
+import styles from "./App.module.css";
+
 export default function App() {
   return (
     <>
-      <h1 className="title">App Component</h1>
+      <h1 className={styles.title}>App Component</h1>
       <Global />
     </>
   );
