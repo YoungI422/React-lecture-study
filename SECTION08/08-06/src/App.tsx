96797b34772a73f62d5499b327b3f432399162cd
YoungI422 <dlsrudtj@gmail.com>
2026-04-16 02:16:23 +0900

    feat: 8-6 수강 완료

--- SECTION08/08-06/src/App.tsx
+++ SECTION08/08-06/src/App.tsx
@@ -1,7 +1,21 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+  text-decoration: underline;
+`;
+
+const Wrapper = styled.section`
+  padding: 2rem;
+  border: 1px solid red;
+`;
+
 export default function App() {
   return (
     <>
-      <h1>App Component</h1>
+      <Wrapper>
+        <Title>App Component</Title>
+      </Wrapper>
     </>
   );
 }
