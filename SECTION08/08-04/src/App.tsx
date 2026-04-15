303a1fe96b9aa61d0a924c04f1d228cd80117165
YoungI422 <dlsrudtj@gmail.com>
2026-04-16 01:55:03 +0900

    feat: 8-4 수강 완료

--- SECTION08/08-04/src/App.tsx
+++ SECTION08/08-04/src/App.tsx
@@ -1,8 +1,22 @@
+import classNames from "classnames/bind";
 import styles from "./App.module.css";
 export default function App() {
+  const isLoggedIn = true;
+  const cx = classNames.bind(styles);
   return (
     <>
       <h1 className={styles.title}>Class Names</h1>
+      <button className={`${styles.btn} ${styles["is-active"]}`}>버튼</button>
+      <button className={cx("btn", "is-active")}>버튼</button>
+
+      {/* 조건부로 클래스 이름을 적용할 때 */}
+
+      <button
+        className={`${styles.btn} ${isLoggedIn ? styles["is-active"] : ""}`}
+      >
+        버튼
+      </button>
+      <button className={cx("btn", { "is-active": isLoggedIn })}>버튼</button>
     </>
   );
 }
