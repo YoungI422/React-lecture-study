6ee4b2ccbb94e7515fe2a6407dcd0aa40648df22
YoungI422 <dlsrudtj@gmail.com>
2026-04-24 17:09:29 +0900

    08-15 강의 수강

--- SECTION08/08-15/src/App.tsx
+++ SECTION08/08-15/src/App.tsx
@@ -1,7 +1,12 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen gap-4">
+        <button className="button">Primary</button>
+        <button className="bg-green-500 text-white py-3 py-5 rounded border-none cursor-pointer hover:bg-green-600 transition-colors">
+          Secondary
+        </button>
+      </div>
     </>
   );
 }
