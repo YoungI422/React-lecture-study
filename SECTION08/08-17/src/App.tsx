4247539302a6f6f5827afb8c762e1785414a1e40
YoungI422 <dlsrudtj@gmail.com>
2026-04-24 17:23:50 +0900

    08-17 강의 수강

--- SECTION08/08-17/src/App.tsx
+++ SECTION08/08-17/src/App.tsx
@@ -1,9 +1,15 @@
+import { twMerge } from "tailwind-merge";
 export default function App() {
+  const isRed = true;
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
         <button className="button">Click Me</button>
-        <button className="btns">Click Me</button>
+        <button
+          className={twMerge("btns", isRed && "bg-red-500 hover:bg-red-600")}
+        >
+          Click Me
+        </button>
       </div>
     </>
   );
