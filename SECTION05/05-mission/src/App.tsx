0636337f6590b4ad16a00eecc9ef31fb893ff4b6
YoungI422 <dlsrudtj@gmail.com>
2026-05-03 02:57:53 +0900

    feat: 5강 미션 완료

--- SECTION05/05-mission/src/App.tsx
+++ SECTION05/05-mission/src/App.tsx
@@ -1,13 +1,12 @@
 import Button from "./Button";
 
 export default function App() {
-  const handler = (message: string) => {
-    alert(message);
-  };
+  const handler = (message: string) => alert(message);
+
   return (
     <>
-      <Button message="Click me!" handler={handler}>
-        ボタン
+      <Button message="한형준" handler={handler}>
+        JUN HAN
       </Button>
     </>
   );
--- SECTION05/05-mission/src/Button.tsx
+++ SECTION05/05-mission/src/Button.tsx
@@ -7,5 +7,9 @@ export default function Button({
   children: React.ReactNode;
   handler: (message: string) => void;
 }) {
-  return <button onClick={() => handler(message)}>{children}</button>;
+  return (
+    <>
+      <button onClick={() => handler(message)}>{children}</button>
+    </>
+  );
 }
