c5ea9333eed9040f16d2df11b0b95482881944e9
YoungI422 <dlsrudtj@gmail.com>
2026-05-03 20:02:58 +0900

    feat: 6강 미션 1

--- SECTION06/06-05/src/App.tsx
+++ SECTION06/06-05/src/App.tsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import HJ from "./Gk9dIwLW4AACU2y.jpg";
+import JH from "./news-p.v1.20250317.c2aac8c6a7f24e36b315493bc15a6c2c_P1.jpg";
+
 export default function App() {
-  return <></>;
+  const [name, setName] = useState("한형준");
+  const [age, setAge] = useState(20);
+  const [gender, setGender] = useState("female");
+  const [pic, setPic] = useState(HJ);
+  const setProfile = () => {
+    setName("Jun Han");
+    setAge(18);
+    setGender("male");
+    setPic(JH);
+  };
+
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      <p>Gender: {gender}</p>
+      <p>
+        <img src={pic} alt="Profile" style={{ width: "200px" }} />
+      </p>
+
+      <button onClick={setProfile}>Update Profile</button>
+    </div>
+  );
 }
