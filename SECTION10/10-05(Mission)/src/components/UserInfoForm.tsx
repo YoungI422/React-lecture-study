c2845c7ab39a37aa74dca32b606ed0994a0fc380
YoungI422 <dlsrudtj@gmail.com>
2026-05-08 07:12:49 +0900

    feat: 10-5 미션 진행 중

--- SECTION10/10-05(Mission)/src/components/UserInfoForm.tsx
+++ SECTION10/10-05(Mission)/src/components/UserInfoForm.tsx
@@ -1,4 +1,39 @@
+import { use, useState } from "react";
+
 export default function UserInfoForm() {
+  const [name, setName] = useState("");
+  const [email, setemail] = useState("");
+  const [gender, setGender] = useState("남성");
+  const [skillsState, setSkillsState] = useState({
+    JavaScript: false,
+    React: false,
+    "Node.js": false,
+  });
+  const [bio, setBio] = useState("");
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setemail(e.target.value);
+  };
+
+  const handleGenderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGender(e.target.value);
+  };
+
+  const handleSkillsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSkillsState((skillsState) => ({
+      ...skillsState,
+      [e.target.value]: e.target.checked,
+    }));
+  };
+
+  const handleBioChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setBio(e.target.value);
+  };
+
   return (
     <div className="user-info">
       <h1 className="user-info__title">User Information</h1>
@@ -7,24 +42,48 @@ export default function UserInfoForm() {
           <label className="form-group__label" htmlFor="name">
             이름:
           </label>
-          <input className="form-group__input" type="text" id="name" />
+          <input
+            className="form-group__input"
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => handleNameChange(e)}
+          />
         </div>
 
         <div className="form-group">
           <label className="form-group__label" htmlFor="email">
             이메일:
           </label>
-          <input className="form-group__input" type="email" id="email" />
+          <input
+            className="form-group__input"
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
         </div>
 
         <div className="form-group form-group--radio">
           <label className="form-group__label">성별:</label>
           <label className="form-group__radio">
-            <input type="radio" id="male" value="남성" />
+            <input
+              type="radio"
+              id="male"
+              value="남성"
+              checked={gender === "남성"}
+              onChange={handleGenderChange}
+            />
             남성
           </label>
           <label className="form-group__radio">
-            <input type="radio" id="female" value="여성" />
+            <input
+              type="radio"
+              id="female"
+              value="여성"
+              checked={gender === "여성"}
+              onChange={handleGenderChange}
+            />
             여성
           </label>
         </div>
@@ -32,15 +91,30 @@ export default function UserInfoForm() {
         <div className="form-group form-group--checkbox">
           <label className="form-group__label">기술 관심:</label>
           <label className="form-group__checkbox">
-            <input type="checkbox" value="JavaScript" />
+            <input
+              type="checkbox"
+              value="JavaScript"
+              checked={skillsState.JavaScript}
+              onChange={handleSkillsChange}
+            />
             JavaScript
           </label>
           <label className="form-group__checkbox">
-            <input type="checkbox" value="React" />
+            <input
+              type="checkbox"
+              value="React"
+              checked={skillsState.React}
+              onChange={handleSkillsChange}
+            />
             React
           </label>
           <label className="form-group__checkbox">
-            <input type="checkbox" value="Node.js" />
+            <input
+              type="checkbox"
+              value="Node.js"
+              checked={skillsState["Node.js"]}
+              onChange={handleSkillsChange}
+            />
             Node.js
           </label>
         </div>
@@ -63,11 +137,11 @@ export default function UserInfoForm() {
 
       <div className="preview">
         <h2 className="preview__title">실시간 입력값</h2>
-        <p className="preview__item">이름: </p>
-        <p className="preview__item">이메일: </p>
-        <p className="preview__item">성별: </p>
-        <p className="preview__item">기술 관심: </p>
-        <p className="preview__item">자기소개: </p>
+        <p className="preview__item">이름: {name}</p>
+        <p className="preview__item">이메일: {email}</p>
+        <p className="preview__item">성별: {gender}</p>
+        <p className="preview__item">기술 관심: {skillsState.join(", ")}</p>
+        <p className="preview__item">자기소개: {bio}</p>
       </div>
     </div>
   );
