49998746e5d2105cc0237d9468c94480a8841d09
YoungI422 <dlsrudtj@gmail.com>
2026-05-08 01:33:37 +0900

    feat: 10-4강 수강 완료

--- SECTION10/10-04/src/components/controlled/Textarea.tsx
+++ SECTION10/10-04/src/components/controlled/Textarea.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 export default function Textarea() {
+  const [text, setText] = useState("");
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
   return (
     <div>
-      <textarea name="desc" />
+      <textarea name="desc" value={text} onChange={handleTextChange} />
       <p>입력된 텍스트:</p>
     </div>
   );
