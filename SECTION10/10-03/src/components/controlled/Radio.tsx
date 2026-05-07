cfc5b4dd3a8f8e7fec3ea9e182eae82d754c7328
YoungI422 <dlsrudtj@gmail.com>
2026-05-08 01:26:37 +0900

    feat: 10-3강 수강 완료

--- SECTION10/10-03/src/components/controlled/Radio.tsx
+++ SECTION10/10-03/src/components/controlled/Radio.tsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
+
 export default function Radio() {
+  const [selectedOption, setSelectedOption] = useState("option1");
+  const handleSelectedOptionChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setSelectedOption(event.target.value);
+  };
   return (
     <div>
       <label>
-        <input type="radio" name="option" value="option1" />
+        <input
+          type="radio"
+          name="option"
+          value="option1"
+          checked={selectedOption === "option1"}
+          onChange={handleSelectedOptionChange}
+        />
         옵션 1
       </label>
       <label>
-        <input type="radio" name="option" value="option2" />
+        <input
+          type="radio"
+          name="option"
+          value="option2"
+          checked={selectedOption === "option2"}
+          onChange={handleSelectedOptionChange}
+        />
         옵션 2
       </label>
       <label>
-        <input type="radio" name="option" value="option3" />
+        <input
+          type="radio"
+          name="option"
+          value="option3"
+          checked={selectedOption === "option3"}
+          onChange={handleSelectedOptionChange}
+        />
         옵션 3
       </label>
     </div>
