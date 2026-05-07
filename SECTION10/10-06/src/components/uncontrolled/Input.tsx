1e86561f6fafda51cfb668e5bf3fed94fb4ca87a
YoungI422 <dlsrudtj@gmail.com>
2026-05-08 07:35:07 +0900

    feat: 10-6 수강 완료

--- SECTION10/10-06/src/components/uncontrolled/Input.tsx
+++ SECTION10/10-06/src/components/uncontrolled/Input.tsx
@@ -1,7 +1,18 @@
+import { useRef } from "react";
+
 export default function Input() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const pwRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(inputRef.current?.value);
+    console.log(pwRef.current?.value);
+  };
+
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" ref={inputRef} />
+      <input type="password" ref={pwRef} />
       <button type="submit">Submit</button>
     </form>
   );
