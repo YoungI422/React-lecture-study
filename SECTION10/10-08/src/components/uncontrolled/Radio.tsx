49e3a746613e4ddae236f424f0c86dc7d5cae7e1
YoungI422 <dlsrudtj@gmail.com>
2026-05-08 08:10:17 +0900

    feat: 10-8강 수강 완료

--- SECTION10/10-08/src/components/uncontrolled/Radio.tsx
+++ SECTION10/10-08/src/components/uncontrolled/Radio.tsx
@@ -1,6 +1,17 @@
+import { useRef } from "react";
+
 export default function Radio() {
+  const formElRef = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(formElRef.current!);
+    const selectedOption = formData.get("option");
+    console.log(selectedOption);
+  };
+
   return (
-    <form>
+    <form ref={formElRef} onSubmit={handleSubmit}>
       <div>
         <label>
           <input type="radio" name="option" value="option1" />
